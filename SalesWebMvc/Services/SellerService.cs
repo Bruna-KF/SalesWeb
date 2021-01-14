d8756e70ff12e791221a8cb2d2b830f7c8609253
Bruna Fonseca <bk.fonseca29@gmail.com>
2021-01-14 20:47:21 +0100

    Delete seller

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -3,18 +3,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
 namespace SalesWebMvc.Services
 {
     public class SellerService
     {
         private readonly SalesWebMvcContext _context;
-
         public SellerService(SalesWebMvcContext context)
         {
             _context = context;
         }
-
         public List<Seller> FindAll()
         {
             return _context.Seller.ToList();
@@ -22,6 +19,18 @@ namespace SalesWebMvc.Services
         public void Insert(Seller obj)
         {
             _context.Add(obj);
+            _context.SaveChanges();
+        }
+
+        public Seller FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
             _context.SaveChanges();
         }
     }
